29591e29a95f64bb6b39f9b99d49cb1d2c951d0b
Srivatsa Katta <vatsa.katta@gmail.com>
2016-12-23 23:52:56 -0500

    Fixed the docker config file from host and container

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -3,5 +3,5 @@
 docker run --name jenkins -d \
       -v /var/run/docker.sock:/var/run/docker.sock \
       -v /var/jenkins_home \
-      -v ~/.docker/config.json:/var/jenkins_home/config.json \
+      -v ~/.docker/config.json:/root/.docker/config.json \
       -p 8080:8080 jenkins
