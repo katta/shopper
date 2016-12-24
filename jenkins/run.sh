9c58a8625729d54a3f3eb3b8329ac8266d7cf797
Srivatsa Katta <vatsa.katta@gmail.com>
2016-12-23 23:45:22 -0500

    Using docker credentials from host config instead of maven settings.xml

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -2,4 +2,6 @@
 
 docker run --name jenkins -d \
       -v /var/run/docker.sock:/var/run/docker.sock \
-      -v /var/jenkins_home -p 8080:8080 jenkins
+      -v /var/jenkins_home \
+      -v ~/.docker/config.json:/var/jenkins_home/config.json \
+      -p 8080:8080 jenkins
